d005c3c33a649ae1e85d60ec7545615b105a477d
Nithil Sunilkumar <iamnithil@gmail.com>
2020-10-24 15:05:39 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+@app.route('/test', methods=['GET', 'POST'])
+def test():
+    if request.method == 'POST':
+        return 'Return using POST'
+    else:
+        return 'Return using GET'
+
+
+if __name__ == '__main__':
+    app.run()
